ccbf337aab5f16e926148421a85084bb67a94197
Trevor Leach <tleachman1234@gmail.com>
2017-11-11 21:23:37 -0600

    debug styling

--- index.php
+++ index.php
@@ -12,21 +12,23 @@
 	<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
 	<title>Login</title>
 </head>
-<body>
+<body class="is-loading">
+	<div id="wrapper" class="fade-in">
 
-	<nav id="nav">
-		<ul class="links">
-			<li class="active"><a href="index.html">This is Massively</a></li>
-			<li><a href="generic.html">Generic Page</a></li>
-			<li><a href="elements.html">Elements Reference</a></li>
-		</ul>
-		<ul class="icons">
-			<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
-			<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
-			<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
-			<li><a href="#" class="icon fa-github"><span class="label">GitHub</span></a></li>
-		</ul>
-	</nav>
+		<nav id="nav">
+			<ul class="links">
+				<li class="active"><a href="index.html">This is Massively</a></li>
+				<li><a href="generic.html">Generic Page</a></li>
+				<li><a href="elements.html">Elements Reference</a></li>
+			</ul>
+			<ul class="icons">
+				<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+				<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+				<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+				<li><a href="#" class="icon fa-github"><span class="label">GitHub</span></a></li>
+			</ul>
+		</nav>
+	</div>
 
 	<br />
 	<br />
