d3d542cc55916ed93f67fb2112054d48d689cafa
Trevor Leach <tleachman1234@gmail.com>
2017-11-11 21:46:00 -0600

    styling

--- index.php
+++ index.php
@@ -15,15 +15,13 @@
 
 	<nav id="nav">
 		<ul class="links">
-			<li class="active"><a href="index.html">This is Massively</a></li>
-			<li><a href="generic.html">Generic Page</a></li>
-			<li><a href="elements.html">Elements Reference</a></li>
-		</ul>
-		<ul class="icons">
-			<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
-			<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
-			<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
-			<li><a href="#" class="icon fa-github"><span class="label">GitHub</span></a></li>
+			<h2>JokeNet</h2>
+			<li class="active">JokeNet</li>
+			<li class="active"><a href="#">Top Jokes</a></li>
+			<li><a href="#recent">Recent Jokes</a></li>
+			<li><a href="#post">Post a Joke</a></li>
+			<li><a href="#profile">Profile</a></li>
+			<li><a href="#about">About</a></li>
 		</ul>
 	</nav>
 
