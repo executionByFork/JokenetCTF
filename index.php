c7d8e67bcc969860ef6df4081094c2b37efc5bc3
Trevor Leach <tleachman1234@gmail.com>
2017-11-11 21:21:49 -0600

    rip navbar

--- index.php
+++ index.php
@@ -6,22 +6,38 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link rel="stylesheet" type="text/css" href="style/style.css" />
-<title>Login</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<link rel="stylesheet" type="text/css" href="style/style.css" />
+	<link rel="stylesheet" href="assets/css/main.css" />
+	<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	<title>Login</title>
 </head>
 <body>
 
-<br />
-<br />
-<br />
-<center><h3>Login</h3></center>
-<form action="authenticate.php" method="POST" class="login">
-  <input type="text" placeholder="Username" name="username">
-  <input type="password" placeholder="Password" name="password">
-  <input type="submit" value="Sign In">
-  <a href="createUser.php?r=login" class="forgot">Create Account</a>
-</form>
+	<nav id="nav">
+		<ul class="links">
+			<li class="active"><a href="index.html">This is Massively</a></li>
+			<li><a href="generic.html">Generic Page</a></li>
+			<li><a href="elements.html">Elements Reference</a></li>
+		</ul>
+		<ul class="icons">
+			<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+			<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+			<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+			<li><a href="#" class="icon fa-github"><span class="label">GitHub</span></a></li>
+		</ul>
+	</nav>
+
+	<br />
+	<br />
+	<br />
+	<center><h3>Login</h3></center>
+	<form action="authenticate.php" method="POST" class="login">
+	  <input type="text" placeholder="Username" name="username">
+	  <input type="password" placeholder="Password" name="password">
+	  <input type="submit" value="Sign In">
+	  <a href="createUser.php?r=login" class="forgot">Create Account</a>
+	</form>
 
 </body>
 </html>
