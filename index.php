d3c603a0cc200b329eb83984d468def3459f9d57
Trevor Leach <tleachman1234@gmail.com>
2017-11-11 21:34:45 -0600

    switch to w3 navbar.css

--- index.php
+++ index.php
@@ -8,27 +8,24 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<link rel="stylesheet" type="text/css" href="style/style.css" />
-	<link rel="stylesheet" href="style/main.css" />
-	<noscript><link rel="stylesheet" href="style/noscript.css" /></noscript>
+	<link rel="stylesheet" href="style/navbar.css" />
 	<title>Login</title>
 </head>
-<body class="is-loading">
-	<div id="wrapper" class="fade-in">
+<body>
 
-		<nav id="nav">
-			<ul class="links">
-				<li class="active"><a href="index.html">This is Massively</a></li>
-				<li><a href="generic.html">Generic Page</a></li>
-				<li><a href="elements.html">Elements Reference</a></li>
-			</ul>
-			<ul class="icons">
-				<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
-				<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
-				<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
-				<li><a href="#" class="icon fa-github"><span class="label">GitHub</span></a></li>
-			</ul>
-		</nav>
-	</div>
+	<nav id="nav">
+		<ul class="links">
+			<li class="active"><a href="index.html">This is Massively</a></li>
+			<li><a href="generic.html">Generic Page</a></li>
+			<li><a href="elements.html">Elements Reference</a></li>
+		</ul>
+		<ul class="icons">
+			<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+			<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+			<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+			<li><a href="#" class="icon fa-github"><span class="label">GitHub</span></a></li>
+		</ul>
+	</nav>
 
 	<br />
 	<br />
