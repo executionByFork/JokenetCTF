77f71bafe10de7098cf3036f1381d8bc9004d423
Trevor Leach <tleachman1234@gmail.com>
2017-11-11 21:06:25 -0600

    add styling

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link rel="stylesheet" type="text/css" href="style.css" />
+<link rel="stylesheet" type="text/css" href="style/style.css" />
 <title>Login</title>
 </head>
 <body>
