d16ead4d9c7561a42b8e30ffceb0a45a42b1e51c
Trevor Leach <tleachman1234@gmail.com>
2017-11-11 22:29:18 -0600

    debug

--- JokeNet/index.php
+++ JokeNet/index.php
@@ -14,7 +14,19 @@
 <body>
 
 <?php
-	include "/JokeNet/navbar.php";
+echo '
+	<nav id="nav">
+		<ul class="links">
+			<h2>JokeNet</h2>
+			<li class="active"><a href="#top">Top Jokes</a></li>
+			<li><a href="#recent">Recent Jokes</a></li>
+			<li><a href="#post">Post a Joke</a></li>
+			<li><a href="#profile">Profile</a></li>
+			<li><a href="#about">About</a></li>
+		</ul>
+	</nav>
+	';
+	//include "/JokeNet/navbar.php";
 ?>
 
 </body>
