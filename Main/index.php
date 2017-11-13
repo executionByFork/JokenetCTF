d5cc0bf5e487be7038559d2d5a891a54f301a3b0
Trevor Leach <tleachman1234@gmail.com>
2017-11-12 23:07:05 -0600

    test login forms

--- Main/index.php
+++ Main/index.php
@@ -17,6 +17,18 @@
 
 	<!-- Official Login -->
 
+	<div class="wrapper">
+    <form class="form-signin">       
+      <h2 class="form-signin-heading">Please login</h2>
+      <input type="text" class="form-control" name="username" placeholder="Email Address" required="" autofocus="" />
+      <input type="password" class="form-control" name="password" placeholder="Password" required=""/>      
+      <label class="checkbox">
+        <input type="checkbox" value="remember-me" id="rememberMe" name="rememberMe"> Remember me
+      </label>
+      <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>   
+    </form>
+  </div>
+
 	<center><h3>Login</h3></center>
 	<form action="authenticate.php" method="POST" class="login">
 	  <input type="text" placeholder="Username" name="username">
