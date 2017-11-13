e8b976fa3a2bba754642d1654a53a6f874ae3290
Trevor Leach <tleachman1234@gmail.com>
2017-11-13 17:22:09 -0600

    test login forms

--- Main/index.php
+++ Main/index.php
@@ -7,6 +7,7 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<link rel="stylesheet" type="text/css" href="/style/style.css" />
+	<link rel="stylesheet" type="text/css" href="/style/styleLogin.css" />
 	<title>Login</title>
 </head>
 <body>
@@ -17,21 +18,33 @@
 
 	<!-- Official Login -->
 
+	<center><h2 class="form-signin-heading">Sign in Here</h2></center>
 	<div class="wrapper">
-    <form class="form-signin">       
-      <h2 class="form-signin-heading">Please login</h2>
-      <input type="text" class="form-control" name="username" placeholder="Email Address" required="" autofocus="" />
-      <input type="password" class="form-control" name="password" placeholder="Password" required=""/>
-      <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>   
+		<form class="form-signin">
+			<input type="text" class="form-control" name="username" placeholder="Username" required="" />
+			<input type="password" class="form-control" name="password" placeholder="Password" required=""/>
+			<button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
+		</form>
+	</div>
+
+	<div id="login">
+    <form name='form-login'>
+      <span class="fontawesome-user"></span>
+        <input type="text" id="user" placeholder="Username" />
+     
+      <span class="fontawesome-lock"></span>
+        <input type="password" id"pass" placeholder="Password" />
+      
+      <input type="submit" value="Login" />
     </form>
   </div>
 
 	<center><h3>Login</h3></center>
 	<form action="authenticate.php" method="POST" class="jokeNetLogin">
-	  <input type="text" placeholder="Username" name="username">
-	  <input type="password" placeholder="Password" name="password">
-	  <input type="submit" value="Sign In">
-	  <a href="createUser.php?r=login" class="forgot">Create Account</a>
+		<input type="text" placeholder="Username" name="username">
+		<input type="password" placeholder="Password" name="password">
+		<input type="submit" value="Sign In">
+		<a href="createUser.php?r=login" class="forgot">Create Account</a>
 	</form>
 
 	<!-- Flag Submission -->
