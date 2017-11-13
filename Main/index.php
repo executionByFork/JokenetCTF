ac40be2fd20a8cdfe5de4983945182ba126987be
Trevor Leach <tleachman1234@gmail.com>
2017-11-13 17:55:41 -0600

    debug

--- Main/index.php
+++ Main/index.php
@@ -6,7 +6,6 @@
 <html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<link rel="stylesheet" type="text/css" href="/style/style.css" />
 	<link rel="stylesheet" type="text/css" href="/style/styleLogin.css" />
 	<title>Login</title>
 </head>
