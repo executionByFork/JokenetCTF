548ce6911dd8ba497c5d476f4ede2ccc0aa15450
Trevor Leach <tleachman1234@gmail.com>
2017-11-14 21:02:21 -0600

    INCLUDE the css file

--- Main/leaderboards.php
+++ Main/leaderboards.php
@@ -18,6 +18,7 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<link rel="stylesheet" type="text/css" href="/style/styling.css" />
+	<link rel="stylesheet" type="text/css" href="/style/leaderboard.css" />
 	<title>CTF Main Page</title>
 </head>
 <body>
