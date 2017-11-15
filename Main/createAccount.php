26b1f1960fbe08f3e2a98aa0ae73bf2d980f8915
Trevor Leach <tleachman1234@gmail.com>
2017-11-14 20:16:28 -0600

    errorfix

--- Main/createAccount.php
+++ Main/createAccount.php
@@ -84,7 +84,7 @@
 
 	//session_regenerate_id();
 	$_SESSION['logged'] = 1;
-	$_SESSION['username'] = $username, ENT_QUOTES, 'UTF-8';
+	$_SESSION['username'] = $username;
 
 	header("Location: /Main/main.php");
 	exit();
