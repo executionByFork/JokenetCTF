72664349f39676e3c148b46cee50fab68fe35414
Trevor Leach <tleachman1234@gmail.com>
2017-11-15 20:39:38 -0600

    flesh out hints.php

--- Main/hints.php
+++ Main/hints.php
@@ -11,6 +11,63 @@
 		echo "<div id='error'><b>" . $_SESSION['ERROR'] . "</b></div>";
     unset($_SESSION['ERROR']);
 	}
+
+	include "../mysql.php";
+
+	$stmt = $conn->stmt_init();
+	$query = "SELECT `flag1`, `flag2`, `flag3`, `flag4`,
+									 `flag5`, `flag6`, `flag7`, `flag8`, `flag9`,
+									 `hint1`, `hint2`, `hint3`, `hint4`,
+									 `hint5`, `hint6`, `hint7`, `hint8`, `hint9` 
+						FROM `users` WHERE `username` = ?";
+
+	if( !$stmt->prepare($query) ) {
+		$_SESSION['ERROR'] = "Problem preparing SQL statement";
+		header("Location: /Main/createAccount.php");
+		die();
+	}
+	$stmt->bind_param("s", $_SESSION['username']);
+	if (!$stmt->execute()){
+		$_SESSION['ERROR'] = "Error executing SQL statement";
+		header("Location: /Main/createAccount.php");
+		die();
+	}
+	$stmt->store_result();
+	if ( !$stmt->num_rows ) {
+		$_SESSION['ERROR'] = "Fatal Error when retrieving user data from database";
+		header("Location: /Main/createAccount.php");
+		die();
+	}
+
+	$stmt->bind_result($f1, $f2, $f3, $f4, $f5, $f6, $f7, $f8, $f9, 
+										 $h1, $h2, $h3, $h4, $h5, $h6, $h7, $h8, $h9);
+
+	if($_POST['Hint1']) $h1 = 1;
+	if($_POST['Hint2']) $h2 = 1;
+	if($_POST['Hint3']) $h3 = 1;
+	if($_POST['Hint4']) $h4 = 1;
+	if($_POST['Hint5']) $h5 = 1;
+	if($_POST['Hint6']) $h6 = 1;
+	if($_POST['Hint7']) $h7 = 1;
+	if($_POST['Hint8']) $h8 = 1;
+	if($_POST['Hint9']) $h9 = 1;
+
+	$query = "UPDATE `users`
+						SET `hint1` = ?, `hint2` = ?, `hint3` = ?, `hint4` = ?,
+								`hint5` = ?, `hint6` = ?, `hint7` = ?, `hint8` = ?, `hint9` = ? 
+						WHERE `username` = ?";
+
+	if( !$stmt->prepare($query) ) {
+		$_SESSION['ERROR'] = "Problem preparing SQL statement";
+		header("Location: /Main/createAccount.php");
+		die();
+	}
+	$stmt->bind_param("iiiiiiiii", $h1, $h2, $h3, $h4, $h5, $h6, $h7, $h8, $h9);
+	if (!$stmt->execute()){
+		$_SESSION['ERROR'] = "Error executing SQL statement";
+		header("Location: /Main/createAccount.php");
+		die();
+	}
 ?>
 
 <!DOCTYPE html>
@@ -37,12 +94,12 @@
 	</center>
 
 	<div id="main">
-		<form action="startChallenge.php" method="POST">
+		<form action="hints.php" method="POST">
 			<h2>Clue 1:</h2>
 			<h3><i>Clue 1 here</i></h3>
 			<input name="Hint1" type="submit" value="Unlock Hint 1" />
 			<?php
-				print "<i>Here is the Hint text</i>"
+				if($f1 || $h1) print "<i>Here is the Hint text</i>";
 			?>
 			<hr />
 			<br />
@@ -50,7 +107,7 @@
 			<h3><i>Clue 2 here</i></h3>
 			<input name="Hint2" type="submit" value="Unlock Hint 2" />
 			<?php
-				print "<i>Here is the Hint text</i>"
+				if($f2 || $h2) print "<i>Here is the Hint text</i>";
 			?>
 			<hr />
 			<br />
@@ -58,7 +115,7 @@
 			<h3><i>Clue 3 here</i></h3>
 			<input name="Hint3" type="submit" value="Unlock Hint 3" />
 			<?php
-				print "<i>Here is the Hint text</i>"
+				if($f3 || $h3) print "<i>Here is the Hint text</i>";
 			?>
 			<hr />
 			<br />
@@ -66,7 +123,7 @@
 			<h3><i>Clue 4 here</i></h3>
 			<input name="Hint4" type="submit" value="Unlock Hint 4" />
 			<?php
-				print "<i>Here is the Hint text</i>"
+				if($f4 || $h4) print "<i>Here is the Hint text</i>";
 			?>
 			<hr />
 			<br />
@@ -74,7 +131,7 @@
 			<h3><i>Clue 5 here</i></h3>
 			<input name="Hint5" type="submit" value="Unlock Hint 5" />
 			<?php
-				print "<i>Here is the Hint text</i>"
+				if($f5 || $h5) print "<i>Here is the Hint text</i>";
 			?>
 			<hr />
 			<br />
@@ -82,7 +139,7 @@
 			<h3><i>Clue 6 here</i></h3>
 			<input name="Hint6" type="submit" value="Unlock Hint 6" />
 			<?php
-				print "<i>Here is the Hint text</i>"
+				if($f6 || $h6) print "<i>Here is the Hint text</i>";
 			?>
 			<hr />
 			<br />
@@ -90,7 +147,7 @@
 			<h3><i>Clue 7 here</i></h3>
 			<input name="Hint7" type="submit" value="Unlock Hint 7" />
 			<?php
-				print "<i>Here is the Hint text</i>"
+				if($f7 || $h7) print "<i>Here is the Hint text</i>";
 			?>
 			<hr />
 			<br />
@@ -98,7 +155,7 @@
 			<h3><i>Clue 8 here</i></h3>
 			<input name="Hint8" type="submit" value="Unlock Hint 8" />
 			<?php
-				print "<i>Here is the Hint text</i>"
+				if($f8 || $h8) print "<i>Here is the Hint text</i>";
 			?>
 			<hr />
 			<br />
@@ -106,7 +163,7 @@
 			<h3><i>Clue 9 here</i></h3>
 			<input name="Hint9" type="submit" value="Unlock Hint 9" />
 			<?php
-				print "<i>Here is the Hint text</i>"
+				if($f9 || $h9) print "<i>Here is the Hint text</i>";
 			?>
 			<hr />
 		</form>
--- Main/logout.php
+++ Main/logout.php
@@ -3,7 +3,6 @@
 
 	$_SESSION['logged'] = 0;
   unset($_SESSION['username']);
-  unset($_SESSION['ERROR']);
 
 	header("Location: /Main/authenticate.php");
 	exit();
