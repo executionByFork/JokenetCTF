f7c1b92e9da1f6c5e3b034ecf58e5287d9b6e94b
Trevor Leach <tleachman1234@gmail.com>
2017-11-16 20:16:07 -0600

    style postJoke.php

---
+++ JokeNet/postJoke.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<link rel="stylesheet" type="text/css" href="/style/jokeStylez.css" />
+	<link rel="stylesheet" href="/style/navbar.css" />
+	<title>Top Jokes - JokeNet</title>
+</head>
+<body>
+
+	<?php
+		include "../mysql.php";
+		$highlightButton = 1;
+		include "navbar.php";
+	?>
+	<br />
+	<br />
+
+	<textarea id="addJoke"></textarea>
+
+</body>
+</html>
