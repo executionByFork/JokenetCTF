7fb976eec40bb2f31ac861a1369809377fea18b5
Trevor Leach <tleachman1234@gmail.com>
2017-11-17 17:10:53 -0600

    implement joke posting

--- JokeNet/postJoke.php
+++ JokeNet/postJoke.php
@@ -15,15 +15,49 @@
 	?>
 	<br />
 	<br />
-	
-	<form action="" method="POST">
-		<textarea name="jokeText"></textarea>
-		<input type="submit" name="post" value="Post to JokeNet">
-	</form>
+
+	<div class="jokePost">
+		<form action="" method="POST">
+			<textarea name="jokeText"></textarea><br />
+			<input type="submit" name="post" value="Post to JokeNet">
+		</form>
+	</div>
 
 </body>
 </html>
 
 <?php
-	
+	if ( !isset($_POST['post']) ) {
+		die();
+	}
+
+	$jokeText = (array_key_exists('jokeText', $_POST) && is_string($_POST['jokeText']))
+											? $_POST['jokeText'] : '';
+
+	if (empty($jokeText)) {
+		print "<script type=\"text/javascript\">
+             alert(\"You didn't write a joke!\");
+           </script>";
+		die();
+	}
+
+	$query = "INSERT INTO `jokes`
+						(`joke`, `postedBy`, `rating`, `numVotes`, `timeStamp`)
+						VALUES
+						(?, ?, 0, 0, now())"
+
+	if( !$stmt->prepare($query) ) {
+		print "<script type=\"text/javascript\">
+             alert(\"Problem preparing SQL statement\");
+           </script>";
+		die();
+	}
+
+	$stmt->bind_param("ss", $jokeText, $_COOKIE["username"]);
+	if ( !$stmt->execute() ) {
+		print "<script type=\"text/javascript\">
+             alert(\"Problem executing SQL statement\");
+           </script>";
+		die();
+	}
 ?>
