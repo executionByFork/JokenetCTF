6b7f8f7d4df5e4af739ce4d3060932fbf182617a
Trevor Leach <tleachman1234@gmail.com>
2017-11-16 18:24:59 -0600

    style joke post test

--- JokeNet/navbar.php
+++ JokeNet/navbar.php
@@ -1,14 +1,32 @@
 <?php
 
+switch ($highlightButton) {
+	case 1:
+		$top = "active";
+		break;
+	case 2:
+		$recent = "active";
+		break;
+	case 3:
+		$post = "active";
+		break;
+	case 4:
+		$profile = "active";
+		break;
+	case 5:
+		$about = "active";
+		break;
+}
+
 echo '
 	<nav id="nav">
 		<ul class="links">
 			<h2>JokeNet</h2>
-			<li class="active"><a href="#top">Top Jokes</a></li>
-			<li><a href="#recent">Recent Jokes</a></li>
-			<li><a href="#post">Post a Joke</a></li>
-			<li><a href="#profile">Profile</a></li>
-			<li><a href="#about">About</a></li>
+			<li class="$top"><a href="#top">Top Jokes</a></li>
+			<li class="$recent"><a href="#recent">Recent Jokes</a></li>
+			<li class="$post"><a href="#post">Post a Joke</a></li>
+			<li class="$profile"><a href="#profile">Profile</a></li>
+			<li class="$about"><a href="#about">About</a></li>
 		</ul>
 		<div id="topRight">
 			<a href="/JokeNet/logout.php"><button id="logout"><b>Logout</b></button></a>
--- JokeNet/topJokes.php
+++ JokeNet/topJokes.php
@@ -9,9 +9,32 @@
 <body>
 
 	<?php
-		include "/mysql.php";
+		include "../mysql.php";
+		$highlightButton = 1;
 		include "navbar.php";
 	?>
+	<br />
+	<br />
+
+	<div class="jokePost">
+		<b>Posted by: <a href="profile.php?user=James">James</a></b>
+		<hr />
+		This is my joke <br />
+		I hope you like it
+		-- James
+		<form action="vote.php" method="POST">
+			<select>
+			  <option value="0" selected>0</option>
+			  <option value="1">1</option>
+			  <option value="2">2</option>
+			  <option value="3">3</option>
+			  <option value="4">4</option>
+			  <option value="5">5</option>
+				<input type="submit" name="vote" value="Vote!">
+				<input type="hidden" name="jokeNumber" value="jokeIDhere">
+			</select>
+		</form>
+	</div>
 
 
 </body>
