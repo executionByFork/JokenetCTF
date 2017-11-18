e25eeee7ce7b0f9420563e11a815fe06c5ed1328
Trevor Leach <tleachman1234@gmail.com>
2017-11-18 00:48:41 -0600

    temp disable login check while editing

--- JokeNet/profile.php
+++ JokeNet/profile.php
@@ -5,7 +5,7 @@
   if (!$_SESSION['logged']) {
     $_SESSION['error'] = 1;
     $_SESSION['msg'] = "You must be logged in to visit that page!";
-    header("Location: /Main/authenticate.php");
+    //header("Location: /Main/authenticate.php");
     die();
   }
 
