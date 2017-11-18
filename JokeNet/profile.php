574e850783993ff8b113459bf81bfc11c5dfadfc
Trevor Leach <tleachman1234@gmail.com>
2017-11-17 19:50:34 -0600

    rip profile css

---
+++ JokeNet/profile.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Profile</title>
+  <link rel="stylesheet" type="text/css" href="/style/profile.css" />
+</head>
+<body>
+  <div class="n-profile-bar">
+  <div class="name">
+    <h3>Ahmet Tek</h3>
+    <p> Analyst Web Developer </p>
+  </div>
+  <div class="about">
+    <div class="my">
+      <img src="http://profile.ak.fbcdn.net/hprofile-ak-prn1/c50.50.629.629/s160x160/72119_100897860077123_541454148_n.jpg" />
+      <p>Based in</p>
+      <p class="link">Turkey, Istanbul</p>
+    </div>
+    <div class="job">
+     <img src="https://assets.nexum.com.tr/Upload/ContentImage/da8ed0da-057b-454b-988c-108ba9e5d5c7/hakkimizda-2.jpg" />
+      <p>Web Developer at</p>
+      <p class="link"> Nexum</p>
+    </div>
+  </div>
+  <div class="n-contact">
+    <ul>
+      <li class="fb"><a href="#">Facebook</a></li>
+      <li class="dribble"><a href="#">Dribble</a></li>
+      <li class="twitter"><a href="#">Twitter</a></li>
+      <li class="github"><a href="#">Rss</a></li>
+    </ul>
+  </div>
+</div>
+</body>
+</html>
