b06512443be060c9b3efcfebc46e30e40c130e17
Trevor Leach <tleachman1234@gmail.com>
2017-11-18 00:52:40 -0600

    profile styling

--- JokeNet/profile.php
+++ JokeNet/profile.php
@@ -32,20 +32,6 @@
     <div class="name">
       <h3>Username</h3>
     </div>
-    <!--
-    <div class="about">
-      <div class="my">
-        <img src="http://profile.ak.fbcdn.net/hprofile-ak-prn1/c50.50.629.629/s160x160/72119_100897860077123_541454148_n.jpg" />
-        <p>Based in</p>
-        <p class="link">Turkey, Istanbul</p>
-      </div>
-      <div class="job">
-       <img src="https://assets.nexum.com.tr/Upload/ContentImage/da8ed0da-057b-454b-988c-108ba9e5d5c7/hakkimizda-2.jpg" />
-        <p>Web Developer at</p>
-        <p class="link"> Nexum</p>
-      </div>
-    </div>
-    -->
     <div class="n-contact">
       <ul>
         <li class="list fb">Email</li>
