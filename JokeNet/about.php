6d83e7f4ff99234ede71e0d861b5a8fe992903ff
Trevor Leach <tleachman1234@gmail.com>
2017-11-19 13:46:17 -0600

    add about.php

--- JokeNet/about.php
+++ JokeNet/about.php
@@ -1,26 +1,36 @@
 <?php
 
 	//check for CTF login
-	session_start();
-	if (!$_SESSION['logged']) {
-		$_SESSION['error'] = 1;
-		$_SESSION['msg'] = "You must be logged in to visit that page!";
-		header("Location: /Main/authenticate.php");
-		die();
-	}
+  session_start();
+  if (!$_SESSION['logged']) {
+    $_SESSION['error'] = 1;
+    $_SESSION['msg'] = "You must be logged in to visit that page!";
+    //header("Location: /Main/authenticate.php");
+    //die();
+  }
 
-	if (!$_COOKIE["logged"]) {
-		header("Location: login.php");
-		die();
-	}
+  if (!$_COOKIE["logged"]) {
+    header("Location: login.php");
+    die();
+  }
+
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>About JokeNet</title>
+  <link rel="stylesheet" type="text/css" href="/style/jokeStylez.css" />
+  <link rel="stylesheet" href="/style/navbar.css" />
 </head>
 <body>
 
+  <?php
+    $highlightButton = 6;
+    include "navbar.php";
+	?>
+	<center>
+		JokeNet Info
+	</center>
 </body>
 </html>
