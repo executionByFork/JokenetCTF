f698ae7d6b762a65873483565dff348d17fe833e
Trevor Leach <tleachman1234@gmail.com>
2017-11-20 10:24:10 -0600

    formatting

--- Main/hints.php
+++ Main/hints.php
@@ -100,7 +100,9 @@
 		<div style="color:red;">
 			<b>WARNING!</b> If you unlock a hint, you can only earn 50% of the points for that flag!
 		</div>
-		**Note: There is at least one flag dealing with XSS vulnerabilities. Due to the amount of damage these attacks can cause, I can't actually allow the JavaScript to run, so everything is sanitized and I'm simply checking the input to classify it as XSS. Because of this, it might be a bit harder to recognize where JokeNet might be "vulnerable" to XSS, and my script will only pick up basic JS injection, so using obscure injection strings won't present you with a Flag. <br />
+		<div id="rules">
+			**Note: There is at least one flag dealing with XSS vulnerabilities. Due to the amount of damage these attacks can cause, I can't actually allow the JavaScript to run, so everything is sanitized and I'm simply checking the input to classify it as XSS. Because of this, it might be a bit harder to recognize where JokeNet might be "vulnerable" to XSS, and my script will only pick up basic JS injection, so using obscure injection strings won't present you with a Flag.
+		</div>
 	<br />
 	</center>
 
