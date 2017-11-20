3143cb863c8590b8e137e7a1d645b39ea2631d21
Trevor Leach <tleachman1234@gmail.com>
2017-11-20 17:54:01 -0600

    check for noJS

--- JokeNet/JSdisabled.php
+++ JokeNet/JSdisabled.php
@@ -6,7 +6,7 @@
 <body>
 	<?php
 		print "
-			NICE! You've disabled JavaScript and have been redirected here (or you found the <noscript> tag in the code and took a look at this page. But meh.) JokeNet relies on Javascript to notify and in many cases redirect the user, so by disabling JS you've effectively bypassed most of the redirects and popups. You should probably re-enable it though, some of the Flag codes are served to you through alert(); popups and you'll miss them if JavaScript is off. Speaking of Flags... <br />
+			NICE! You've disabled JavaScript and have been redirected here (or you found the <noscript> tag in the HTML and took a look at this page. But meh.) JokeNet relies on Javascript to notify and in many cases redirect the user, so by disabling JS you've effectively bypassed most of the redirects and popups. You should probably re-enable it though, some of the Flag codes are served to you through alert(); popups and you'll miss them if JavaScript is off. Speaking of Flags... <br />
 			<br />
 
 			FLAG-T7JD2HFOIISJK8FBHFUAU0191
