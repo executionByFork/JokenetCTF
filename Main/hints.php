86d2a79ba0906192dbcae942c0371d29cb816f6f
Trevor Leach <tleachman1234@gmail.com>
2017-11-19 22:12:36 -0600

    errorfix

--- Main/hints.php
+++ Main/hints.php
@@ -47,14 +47,14 @@
 	
   unset($query);
 	if($_POST['Hint1'] && !$f1) $query = "UPDATE `users` SET `hint1` = 1 WHERE `username` = ?";
-	elseif($_POST['Hint2'] && !$f2) $query = "UPDATE `users` SET `hint2` = 2 WHERE `username` = ?";
-	elseif($_POST['Hint3'] && !$f3) $query = "UPDATE `users` SET `hint3` = 3 WHERE `username` = ?";
-	elseif($_POST['Hint4'] && !$f4) $query = "UPDATE `users` SET `hint4` = 4 WHERE `username` = ?";
-	elseif($_POST['Hint5'] && !$f5) $query = "UPDATE `users` SET `hint5` = 5 WHERE `username` = ?";
-	elseif($_POST['Hint6'] && !$f6) $query = "UPDATE `users` SET `hint6` = 6 WHERE `username` = ?";
-	elseif($_POST['Hint7'] && !$f7) $query = "UPDATE `users` SET `hint7` = 7 WHERE `username` = ?";
-	elseif($_POST['Hint8'] && !$f8) $query = "UPDATE `users` SET `hint8` = 8 WHERE `username` = ?";
-	elseif($_POST['Hint9'] && !$f9) $query = "UPDATE `users` SET `hint9` = 9 WHERE `username` = ?";
+	elseif($_POST['Hint2'] && !$f2) $query = "UPDATE `users` SET `hint2` = 1 WHERE `username` = ?";
+	elseif($_POST['Hint3'] && !$f3) $query = "UPDATE `users` SET `hint3` = 1 WHERE `username` = ?";
+	elseif($_POST['Hint4'] && !$f4) $query = "UPDATE `users` SET `hint4` = 1 WHERE `username` = ?";
+	elseif($_POST['Hint5'] && !$f5) $query = "UPDATE `users` SET `hint5` = 1 WHERE `username` = ?";
+	elseif($_POST['Hint6'] && !$f6) $query = "UPDATE `users` SET `hint6` = 1 WHERE `username` = ?";
+	elseif($_POST['Hint7'] && !$f7) $query = "UPDATE `users` SET `hint7` = 1 WHERE `username` = ?";
+	elseif($_POST['Hint8'] && !$f8) $query = "UPDATE `users` SET `hint8` = 1 WHERE `username` = ?";
+	elseif($_POST['Hint9'] && !$f9) $query = "UPDATE `users` SET `hint9` = 1 WHERE `username` = ?";
 
 	if($query) {
 		if( !$stmt->prepare($query) ) {
